da113d17859a29efdc19138fd59e0c8a27e4e42e
Adrian Boaventura Silva <104996@PROWAY.TREINA>
2018-07-03 17:29:29 -0300

    .

---
+++ Exercicio01.sql
@@ -0,0 +1,45 @@
+﻿DROP TABLE caracteristicas
+DROP TABLE alunos
+CREATE TABLE alunos(
+	id		INT IDENTITY (1,1),
+	nome	VARCHAR(200),
+	idade   INT
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE caracteristicas(
+	id	      INT IDENTITY (1,1),
+	id_alunos INT,
+	caracteristica VARCHAR (150) NOT NULL
+	FOREIGN KEY (id_alunos) REFERENCES alunos(id)
+);
+
+INSERT INTO alunos(nome,idade) VALUES
+('Alice',18),
+('Sophia',4),
+('Miguel',9),
+('Heitor',23),
+('Valentina',15),
+('Joaquim',49)
+
+INSERT INTO caracteristicas(id_alunos, caracteristica) VALUES
+((SELECT id FROM alunos WHERE nome = 'Heitor'),'Egoísta'),
+((SELECT id FROM alunos WHERE nome = 'Alice'),'Organizado(a)'),
+((SELECT id FROM alunos WHERE nome = 'Sophia'),'Pontual'),
+((SELECT id FROM alunos WHERE nome = 'Miguel'),'Criativo(a)'),
+((SELECT id FROM alunos WHERE nome = 'Heitor'),'Proativo(a)'),
+((SELECT id FROM alunos WHERE nome = 'Alice'),'Altruísta'),
+((SELECT id FROM alunos WHERE nome = 'Valentina'),'Pessimista'),
+((SELECT id FROM alunos WHERE nome = 'Joaquim'),'Flexível modelo'),
+((SELECT id FROM alunos WHERE nome = 'Sophia'),'Observadora'),
+((SELECT id FROM alunos WHERE nome = 'Joaquim'),'Paciente'),
+((SELECT id FROM alunos WHERE nome = 'Heitor'),'Indelicado(a)'),
+((SELECT id FROM alunos WHERE nome = 'Sophia'),'Desobediente'),
+((SELECT id FROM alunos WHERE nome = 'Miguel'),'Intolerante'),
+((SELECT id FROM alunos WHERE nome = 'Joaquim'),'Empático(a)'),
+((SELECT id FROM alunos WHERE nome = 'Sophia'),'Egoísta'),
+((SELECT id FROM alunos WHERE nome = 'Heitor'),'Altruísta'),
+((SELECT id FROM alunos WHERE nome = 'Sophia'),'Sensível')
+
+
+SELECT * FROM caracteristicas
---
+++ Exercicio02.sql
@@ -0,0 +1,36 @@
+﻿DROP TABLE carros;
+DROP TABLE pessoas;
+CREATE TABLE pessoas(
+	id		INT IDENTITY(1,1),
+	nome	VARCHAR(200),
+	cpf		VARCHAR(11)
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE carros(
+	id			INT IDENTITY(1,1),
+	id_pessoas	INT,
+	marca		VARCHAR(150)NOT NULL,
+	modelo		VARCHAR(150)NOT NULL,
+	lancamento	DATE,
+	fabricacao	DATE,
+	motor		VARCHAR(150),
+	cor			VARCHAR(100),
+	preco		DECIMAL(8,2)
+	FOREIGN KEY (id_pessoas) REFERENCES pessoas(id)
+);
+
+INSERT INTO pessoas (nome, cpf) VALUES
+('Abraão Nobre','95232829483'),
+('Severino Braga','87677033300'),
+('Samuel Faria','98927203429'),
+('Florêncio Robalo','36263517425');
+
+INSERT INTO carros(id_pessoas, marca, modelo, lancamento, fabricacao,motor,cor, preco) VALUES
+((SELECT id FROM pessoas WHERE nome = 'Abraão Nobre'),'Volkswagen','Gol','2010','2009','8v Power Flex','Vermelho',18000.00),
+((SELECT id FROM pessoas WHERE nome = 'Severino Braga'),'Fiat','Brava','2000','1999','SX 1.6 16V','Cinza',9000.00),
+((SELECT id FROM pessoas WHERE nome = 'Samuel Faria'),'Renault','Clio','1997','1996','1.0 8v','Verde',5500.00),
+((SELECT id FROM pessoas WHERE nome = 'Florêncio Robalo'),'Volkswagen','Golf','1994','1994','2.0 120cv','Azul',17000.00)
+
+SELECT * FROM  carros
+
